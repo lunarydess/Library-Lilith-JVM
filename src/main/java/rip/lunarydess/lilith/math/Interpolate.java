3f47ba2a4d156eba865a1089bf677b13a77ea617
lunarydess <blank1337@keemail.me>
2024-01-08 02:15:47 +0100

    :sparkles: Interpolate.java :sparkles: Statistics.java

---
+++ src/main/java/rip/lunarydess/lilith/math/Interpolate.java
@@ -0,0 +1,11 @@
+﻿package rip.lunarydess.lilith.math;
+
+public final class Interpolate {
+    public static float lerp(final float min, final float max, final float delta) {
+        return Arithmetics.clamp((1.0F - delta) * min + delta * max, min, max);
+    }
+
+    public static double lerp(final double min, final double max, final double delta) {
+        return Arithmetics.clamp((1.0D - delta) * min + delta * max, min, max);
+    }
+}
---
+++ src/main/java/rip/lunarydess/lilith/math/Statistics.java
@@ -0,0 +1,46 @@
+﻿package rip.lunarydess.lilith.math;
+
+public final class Statistics {
+    public static float median(final float... data) {
+        final int dataLengthHalf = data.length / 2;
+        final float valueAtHalf = data[dataLengthHalf];
+        return data.length % 2 != 0 ? valueAtHalf : (valueAtHalf + data[dataLengthHalf - 1]) * 0.5F;
+    }
+
+    public static double median(final double... data) {
+        final int dataLengthHalf = data.length / 2;
+        final double valueAtHalf = data[dataLengthHalf];
+        return data.length % 2 != 0 ? valueAtHalf : (valueAtHalf + data[dataLengthHalf - 1]) * 0.5F;
+    }
+
+    public static float variance(final float... data) {
+        final int count = data.length;
+        final float sum = Arithmetics.sum(data);
+        final float average = sum / count;
+        float variance = 0.0F;
+        for (final float number : data) {
+            final float base = number - average;
+            variance += (base * base);
+        }
+        return variance / count;
+    }
+
+    public static double variance(final double... data) {
+        final int count = data.length;
+        final double average = Arithmetics.sum(data) / count;
+        double variance = 0.0F;
+        for (final double number : data) {
+            final double base = number - average;
+            variance += (base * base);
+        }
+        return variance / count;
+    }
+
+    public static float standardDeviation(final float... data) {
+        return (float) Math.sqrt(variance(data));
+    }
+
+    public static double standardDeviation(final double... data) {
+        return Math.sqrt(variance(data));
+    }
+}
